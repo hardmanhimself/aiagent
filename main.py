2aaf596c9dc91f6eeea9dede1c7f5a606a6c4750
JD Hardman <josh.hardman98@gmail.com>
2025-07-07 17:46:24 +0100

    function calling

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from google import genai
 from google.genai import types
 from dotenv import load_dotenv
 
+from prompts import system_prompt
+from call_function import available_functions, call_function
+
 
 def main():
     load_dotenv()
@@ -14,7 +17,7 @@ def main():
     if not args:
         print("AI Code Assistant")
         print('\nUsage: python main.py "your prompt here" [--verbose]')
-        print('Example: python main.py "How do I build a calculator app?"')
+        print('Example: python main.py "How do I fix the calculator?"')
         sys.exit(1)
 
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -29,19 +32,39 @@ def main():
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    generate_content(client, messages, verbose)
+    generate_content(client, messages, verbose, available_functions)
 
 
-def generate_content(client, messages, verbose):
+def generate_content(client, messages, verbose, available_functions):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+            ),
     )
     if verbose:
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
-    print("Response:")
-    print(response.text)
+
+    if not response.function_calls:
+        print(response.text)
+
+    function_responses = []
+    for function_call_part in response.function_calls:
+            function_call_result = call_function(function_call_part)
+            if (
+                not function_call_result.parts
+                or not function_call_result.parts[0].function_response.response
+                ):
+                raise Exception("empty function call result")
+            if verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
+            function_responses.append(function_call_result.parts[0].function_response.response)
+
+    if not function_responses:
+        raise Exception("no function responses generated, exiting.")
+    
 
 
 if __name__ == "__main__":
